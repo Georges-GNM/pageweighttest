6539c6467a8aa594d03f4cd628978eb320af358e
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2025-02-25 10:32:21 +0000

    quick tweak

--- app/capiCaller.tsx
+++ app/capiCaller.tsx
@@ -1,8 +1,7 @@
 //make sure to update/remove this eventually!
-const capiKey = "176f1616-1e52-42b3-adeb-11ae72de4efc";
 
 export async function getArticles() {
-  const url = `https://content.guardianapis.com/search?api-key=${capiKey}`;
+  const url = `https://content.guardianapis.com/search?api-key=${process.env.CAPI_KEY}`;
   const response = await fetch(url);
   const reply = await response.json();
 
